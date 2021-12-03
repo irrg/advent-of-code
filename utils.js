480fbe3a32d4d5bbb1d15821b067f255dd5cd9a2
Robb Irrgang <robb@irrg.org>
2021-12-03 09:53:46 -0600

    uh, what?

--- utils.js
+++ utils.js
@@ -6,10 +6,6 @@ const { readFile } = require('fs/promises');
  * @param {Number} day
  * @param {Boolean} example
  * @returns Array
- *
- * You don't actually have to check if your slice has 3 values, because for this puzzle,
- * the sum of only two rolling values will be a lot lower than the last value so it'll
- * get eliminated that way.
  */
 const readInputFile = async (day, example = false) => {
   try {
