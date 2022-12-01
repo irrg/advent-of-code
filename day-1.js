ec94f6eadba73a48b6222663d5bee8a4c8d44b8d
Robb Irrgang <robb@irrg.org>
2022-12-01 17:17:34 -0600

    cleanup

--- day-1.js
+++ day-1.js
@@ -1,26 +1,14 @@
 const utils = require('./utils.js');
 
-const getNHighestValues = (values, n) =>
-    values
-      .sort(function(a, b) {
-        return a - b;
-      })
-      .slice(-n)
-      .reduce((total, i) => total + i, 0);
-
 /**
  * Do the thing.
  */
 const main = async () => {
-  const values = (await utils.readInputFile('day-1', '\n\n'))
-    .map((v) => {
-      return v
-        .split('\n')
-        .reduce((a, subValue) => a + parseInt(subValue), 0);
-    });
+  const values = (await utils.readInputFile('day-1', ['\n\n', '\n']))
+    .map(elf => utils.getSum(elf));
 
-  console.log('Part 1', getNHighestValues(values, 1));
-  console.log('Part 2', getNHighestValues(values, 3));
+  console.log('Part 1', utils.getNHighestValues(values, 1));
+  console.log('Part 2', utils.getNHighestValues(values, 3));
 };
 
 main();
--- utils.js
+++ utils.js
@@ -1,22 +1,56 @@
 const { readFile } = require('fs/promises');
 
 /**
- * Read a file in a promise and split it into lines.
+ * Recursively split an array of values based on one or more delimiters.
  *
- * @param {number} day
- * @param {boolean} example
+ * @param {string} string
+ * @param {Array} delimiters
  * @returns Array
  */
-const readInputFile = async (fileName, split = '\n') => {
+const recursiveSplit = (string, delimiters) => {
+  return delimiters.length
+    ? string.split(delimiters[0]).map(x => recursiveSplit(x, delimiters.slice(1)))
+    : string;
+};
+
+/**
+ * Read a file in a promise and split it into an array based on one or more delimiters.
+ *
+ * @param {string} fileName
+ * @param {Array} delimiters
+ * @returns Array
+ */
+const readInputFile = async (fileName, delimiters = ['\n']) => {
   try {
     const data = await readFile(`./input/${fileName}.txt`, 'utf8');
-    return data.split(split);
+    return delimiters.length
+      ? recursiveSplit(data, delimiters)
+      : data;
   }
   catch(err) {
     console.log(err);
   }
 }
 
+/**
+ * Sum up the values in an array.
+ *
+ * @param {Array} values
+ * @returns number
+ */
+const getSum = (values) => values.reduce((total, i) => total + parseInt(i), 0);
+
+/**
+ * Get the highest n values from an array.
+ *
+ * @param {Array} values
+ * @param {number} n
+ * @returns Array
+ */
+const getNHighestValues = (values, n) => getSum(values.sort((a, b) => a - b).slice(-n));
+
 module.exports = {
   readInputFile,
+  getSum,
+  getNHighestValues,
 };
