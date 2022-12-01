06c3940e8341a6c53ea67a28db0926b52b50337a
Robb Irrgang <robb@irrg.org>
2022-12-01 17:25:43 -0600

    let’s get modular

---
+++ day-1.mjs
@@ -0,0 +1,18 @@
+import {
+  getSum,
+  getNHighestValues,
+  readInputFile
+} from './utils.mjs';
+
+/**
+ * Do the thing.
+ */
+const main = async () => {
+  const values = (await readInputFile('day-1', ['\n\n', '\n']))
+    .map(elf => getSum(elf));
+
+  console.log('Part 1', getNHighestValues(values, 1));
+  console.log('Part 2', getNHighestValues(values, 3));
+};
+
+main();
---
+++ utils.mjs
@@ -0,0 +1,57 @@
+import { readFile }  from 'fs/promises';
+
+/**
+ * Get the highest n values from an array.
+ *
+ * @param {Array} values
+ * @param {number} n
+ * @returns Array
+ */
+const getNHighestValues = (values, n) => getSum(values.sort((a, b) => a - b).slice(-n));
+
+/**
+ * Sum up the values in an array.
+ *
+ * @param {Array} values
+ * @returns number
+ */
+const getSum = (values) => values.reduce((total, i) => total + parseInt(i), 0);
+
+/**
+ * Read a file in a promise and split it into an array based on one or more delimiters.
+ *
+ * @param {string} fileName
+ * @param {Array} delimiters
+ * @returns Array
+ */
+ const readInputFile = async (fileName, delimiters = ['\n']) => {
+  try {
+    const data = await readFile(`./input/${fileName}.txt`, 'utf8');
+    return delimiters.length
+      ? recursiveSplit(data, delimiters)
+      : data;
+  }
+  catch(err) {
+    console.log(err);
+  }
+}
+
+/**
+ * Recursively split an array of values based on one or more delimiters.
+ *
+ * @param {string} string
+ * @param {Array} delimiters
+ * @returns Array
+ */
+ const recursiveSplit = (string, delimiters) => {
+  return delimiters.length
+    ? string.split(delimiters[0]).map(x => recursiveSplit(x, delimiters.slice(1)))
+    : string;
+};
+
+export {
+  getNHighestValues,
+  getSum,
+  readInputFile,
+  recursiveSplit,
+};
